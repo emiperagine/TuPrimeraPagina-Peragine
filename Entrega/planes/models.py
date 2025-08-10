ec3f812cc84118590d237a8df7cf8437f1ecb9b0
Emilio Peragine <eeemipera@gmail.com>
2025-08-10 15:52:14 -0300

    clases creadas en models/planes

--- Entrega/planes/models.py
+++ Entrega/planes/models.py
@@ -1,19 +1,25 @@
 from django.db import models
 
 # Create your models here.
-class Planes(models.Model):
-    plan = models.CharField(max_length=10)
-    precio = models.CharField(max_length=10)
+
+class Clases(models.Model):
+    nombre = models.CharField(max_length=20)
+    descripcion = models.CharField(max_length=100)
+  
+class Plan(models.Model):
+    nombre = models.CharField(max_length=10)
+    precio = models.DecimalField(max_digits=8)
+    clases_incluidas = models.ManyToManyField(Clases, related_name='plan')
 
     def __str__(self):
         return (f'Plan: {self.plan}')
     
-class Beneficio(models.Model):
-    clases_incluidas = models.Charfield(max_length=20)
-  
+    
 
-    def __str__(self):
-        return (f'Clases incluidas en el plan {self.plan}: {self.clases_incluidas}.')
-    
-    
+
+# plan_oro = Planes('Oro', 60000)
+
+# plan_plata = Planes('Plata', 48000)
+
+# plan_bronce = Planes('Bronce', 38000)
 
