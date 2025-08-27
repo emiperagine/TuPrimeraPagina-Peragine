e1fed5c7bff9d73eec436da2394e89ee0ace5d8a
Emilio Peragine <eeemipera@gmail.com>
2025-08-26 21:07:10 -0300

    actualizacion

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from inicio.views import index
+from inicio.views import inicio, armar_plan, detalle_plan, editar_plan
 
 urlpatterns = [
-    path('inicio/', index),
+    path('', inicio, name='inicio'),
+    path('planes/', listado_de_autos, name='listado_de_autos'),
+    path('planes/armar/', armar_plan, name='armar_plan'),
+    path('planes/<int:id_plan>/', detalle_plan, name='detalle_plan'),
+    path('planes/<int:pk>/borrar/', AutoBorrar.as_view(), name='borrar_plan'),
+    path('planes/<int:pk>/actualizar/', AutoActualizar.as_view(), name='actualizar_plan'),
 ]
 
